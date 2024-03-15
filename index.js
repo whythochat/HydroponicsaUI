ae2b6d22879f5ff86fb2962e7b74950a7ffb7d3c
whythochat <abrahamdaniel16901@gmail.com>
2024-03-15 10:37:38 +0530

    update

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 const { app, BrowserWindow } = require('electron');
-const { SerialPort } = require('serialport')
-const { ReadlineParser } = require('@serialport/parser-readline')
-
-const port = new SerialPort({
-  path: '/dev/ttyS0',
-  baudRate: 9600,
-})
-
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-
-function send() {
-  port.write('Hello Uday')
-}
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -25,21 +12,5 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
-  var receivedData = ''
-  port.on('data', data => {
-    // Convert buffer data to string
-    var strData = data.toString();
-    // Concatenate the string data
-    receivedData += strData;
-    // Check if you received a complete message
-    if (receivedData.endsWith('END')) {
-        // Process the complete message
-        console.log('Received message:', receivedData);
-        // Clear the buffer for the next message
-        receivedData = '';
-        send()
-    }
-  });
-
 });
 
