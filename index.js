0129ef189b0336049d2581a106ee5c30b37fa7e4
whythochat <41367501+whythochat@users.noreply.github.com>
2024-03-18 22:25:47 +0530

    update to index.js

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const { app, BrowserWindow } = require('electron');
+const  fs = require('fs');
 
-const spawner = require('child_process').spawn;
+//const spawner = require('child_process').spawn;
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
 
-    fullscreen: true,
-    autoHideMenuBar: true
+    //fullscreen: true,
+    autoHideMenuBar: true,
+    width: 854,
+    height: 480,
+  })
+  fs.readFile('./setup.json', 'utf8', (err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    else{
+      const setup = JSON.parse(data);
+      if(setup.setup === 'not'){
+        win.loadFile('./setup.html');
+      }
+      else{
+        win.loadFile('./index.html');
+      }
+    }
   })
 }
 
 app.whenReady().then(() => {
   createWindow();
-  const pass = 'Hello';
-  const pythonresp = spawner('python', ['./comms.py', pass]);
-  pythonresp.stdout.on('data', (data) => {
-    console.log('Received data:', data.toString());
-  });
+  //const pass = 'Hello';
+  //const pythonresp = spawner('python', ['./comms.py', pass]);
+  //pythonresp.stdout.on('data', (data) => {
+  //  console.log('Received data:', data.toString());
+  //});
 });
 
