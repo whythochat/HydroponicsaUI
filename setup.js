39332787ab3a904b8bf0bb8c615a033716610d30
ConsoleCap45 <tjagan42@gmail.com>
2024-04-15 09:49:06 +0530

    Updated their setup page

--- setup.js
+++ setup.js
@@ -3,8 +3,8 @@ var typeOfLights = '';
 var pumpHour1 = 0;
 var pumpHour2 = 0;
 var timing = 0;
-var lighton= 0;
-var lightoff = 0; 
+var lighton = 0;
+var lightoff = 0;
 
 const tankh = document.getElementById('tank-capacity');
 const pumpCycle = document.getElementById('water-pump-cycle');
@@ -17,10 +17,10 @@ keys.forEach(key => {
     if (key.getAttribute('data-value') === '-1') {
       tankh.value = tankh.value.slice(0, -1);
     }
-    else{
-    const keyValue = key.getAttribute('data-value');
-    tankh.value += keyValue;
-  }
+    else {
+      const keyValue = key.getAttribute('data-value');
+      tankh.value += keyValue;
+    }
   });
 });
 
@@ -45,8 +45,8 @@ function updateValues() {
   var input = document.getElementById('water-pump-cycle').value;
   timing = input.split('/');
   if (timing.length === 2) {
-    pumpHour1=timing[0]
-    pumpHour2=timing[1]
+    pumpHour1 = timing[0]
+    pumpHour2 = timing[1]
   }
 }
 
@@ -55,21 +55,21 @@ keys2.forEach(key => {
     if (key.getAttribute('data-value') === '-1') {
       pumpCycle.value = pumpCycle.value.slice(0, -1);
     }
-    else{
-    const keyValue = key.getAttribute('data-value');
-    pumpCycle.value += keyValue;
-  }
-  updateValues();
+    else {
+      const keyValue = key.getAttribute('data-value');
+      pumpCycle.value += keyValue;
+    }
+    updateValues();
   });
 });
 
 let currentQuestion = 1;
 const questions = document.querySelectorAll('.question');
 
-document.getElementById('lights').addEventListener('change', () =>{
+document.getElementById('lights').addEventListener('change', () => {
   var selectedOption = document.querySelector('input[name="option"]:checked').value;
 
-  switch(selectedOption){
+  switch (selectedOption) {
     case 'artificial':
       console.log('Artificial Lights Selected');
       typeOfLights = 'artificial';
@@ -81,24 +81,54 @@ document.getElementById('lights').addEventListener('change', () =>{
   }
 })
 
+// function showQuestion(questionNumber) {
+//   questions.forEach((question, index) => {
+//     if (index === questionNumber - 1) {
+//       question.style.display = 'block';
+//       setTimeout(() => {
+//         question.style.opacity = '1';
+//         question.style.transform = 'translateX(0)';
+//       }, 10); // Adding a small delay to ensure the slide effect works properly
+//     } else {
+//       setTimeout(() => {
+//         question.style.opacity = '0';
+//         question.style.transform = 'translateX(-100%)';
+//         setTimeout(() => {
+//           question.style.display = 'none';
+//         }, 500); // Hiding after animation is complete
+//       }, 0); // No delay for hiding other questions
+//     }
+//   });
+// }
+
+
 function showQuestion(questionNumber) {
   questions.forEach((question, index) => {
     if (index === questionNumber - 1) {
       question.style.display = 'block';
-      setTimeout(() => {
-        question.style.opacity = '1';
-        question.style.transform = 'translateX(0)';
-      }, 10); // Adding a small delay to ensure the slide effect works properly
+      question.style.opacity = '1';
+      question.style.transform = 'translateX(0)';
     } else {
-      setTimeout(() => {
-        question.style.opacity = '0';
-        question.style.transform = 'translateX(-100%)';
-        setTimeout(() => {
-          question.style.display = 'none';
-        }, 500); // Hiding after animation is complete
-      }, 0); // No delay for hiding other questions
+      question.style.opacity = '0';
+      question.style.transform = 'translateX(-100%)';
+      question.style.display = 'none';
     }
   });
+}
+
+
+function previousQuestion() {
+  if (currentQuestion > 0) {
+    currentQuestion--;
+    showQuestion(currentQuestion);
+  }
+}
+
+function previous_Question() {
+  if (currentQuestion > 0) {
+    currentQuestion = currentQuestion - 2;
+    showQuestion(currentQuestion);
+  }
 }
 
 function nextQuestion() {
@@ -108,97 +138,97 @@ function nextQuestion() {
   }
 }
 
-function skipQuestion(){
+function skipQuestion() {
   if (currentQuestion < questions.length) {
-    currentQuestion+=2;
+    currentQuestion += 2;
     showQuestion(currentQuestion);
   }
 }
 
-function updateph(){ 
-    const interval = setInterval( () => {
+function updateph() {
+  const interval = setInterval(() => {
     var newValue, stat
     fetch('./info.json')
-    .then(response => response.json())
-    .then(data => {
-      newValue = data.pH
-      document.getElementById('phval').innerHTML = newValue;
-      stat = data.pc
-      if (stat == 1) {
-      document.getElementById('loading-icon').style.display = 'none';
-      document.getElementById('tick-mark').style.display = 'block';
-      document.getElementById('phch').disabled = false;
-      clearInterval(interval)
-      }
-    })
-  }, 500);
-}
-
-function updatetds(){ 
-  const interval = setInterval( () => {
-  fetch('./info.json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('tdsval').innerHTML = data.TDS   
-  })
-}, 500);
-}
-
-function tdschk(){ 
-    const interval = setInterval( () => {
-    fetch('./info.json')
-    .then(response => response.json())
-    .then(data => {
-      var newValue = data.TDS
-      var stat = data.tc
-      document.getElementById('tdsval2').innerHTML = newValue + " ppm";
-      if (stat == 1) {
-        document.getElementById('loading-icon2').style.display = 'none';
-        document.getElementById('tick-mark2').style.display = 'block';
-        document.getElementById('tdsch').disabled = false;
-        clearInterval(interval)
+      .then(response => response.json())
+      .then(data => {
+        newValue = data.pH
+        document.getElementById('phval').innerHTML = newValue;
+        stat = data.pc
+        if (stat == 1) {
+          document.getElementById('loading-icon').style.display = 'none';
+          document.getElementById('tick-mark').style.display = 'block';
+          document.getElementById('phch').disabled = false;
+          clearInterval(interval)
         }
-    })
+      })
   }, 500);
 }
 
-function startPhCalibration(){
+function updatetds() {
+  const interval = setInterval(() => {
+    fetch('./info.json')
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('tdsval').innerHTML = data.TDS
+      })
+  }, 500);
+}
+
+function tdschk() {
+  const interval = setInterval(() => {
+    fetch('./info.json')
+      .then(response => response.json())
+      .then(data => {
+        var newValue = data.TDS
+        var stat = data.tc
+        document.getElementById('tdsval2').innerHTML = newValue + " ppm";
+        if (stat == 1) {
+          document.getElementById('loading-icon2').style.display = 'none';
+          document.getElementById('tick-mark2').style.display = 'block';
+          document.getElementById('tdsch').disabled = false;
+          clearInterval(interval)
+        }
+      })
+  }, 500);
+}
+
+function startPhCalibration() {
   window.electronAPI.writeserial([3,
     {
       "status": 2
-  }
+    }
   ]);
 }
 
-function turnLightsOnOff(){
+function turnLightsOnOff() {
   window.electronAPI.writeserial([3,
     {
       "status": 5
-  }
+    }
   ]);
 }
 
-function showTdsValue(){
+function showTdsValue() {
   window.electronAPI.writeserial([3,
     {
       "status": 3
-  }
+    }
   ]);
 }
 
-function startTdsCalibration(){
+function startTdsCalibration() {
   window.electronAPI.writeserial([3,
     {
       "status": 4
-  }
+    }
   ]);
 }
 
-function turnMotorOnOff(){
+function turnMotorOnOff() {
   window.electronAPI.writeserial([3,
     {
       "status": 6
-  }
+    }
   ]);
 }
 
@@ -210,22 +240,22 @@ function submitForm() {
   window.electronAPI.writeserial([3,
     {
       "status": 1,
-      "WL":parseInt(waterlevel,10),
-      "l_time":[parseInt(lighton, 10), parseInt(lightoff, 10)],
-      "m_time":[parseInt(pumpHour1, 10), parseInt(pumpHour2, 10)]
-  }
+      "WL": parseInt(waterlevel, 10),
+      "l_time": [parseInt(lighton, 10), parseInt(lightoff, 10)],
+      "m_time": [parseInt(pumpHour1, 10), parseInt(pumpHour2, 10)]
+    }
   ]);
   setTimeout(startPhCalibration, 500);
 }
 
-function finish(){
+function finish() {
   window.electronAPI.saveconfig(1);
   window.electronAPI.writeserial([3,
     {
       "status": 0
-  }
+    }
   ]);
-  window.location.href= "index.html";  
+  window.location.href = "index.html";
 }
 
 // Show the first question initially
