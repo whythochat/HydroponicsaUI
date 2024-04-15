f3850c0e6333fdf085e360de3083e28a0904a634
Abraham Daniel <41367501+whythochat@users.noreply.github.com>
2024-04-15 20:55:14 +0530

    commit

--- disp.js
+++ disp.js
@@ -344,6 +344,25 @@ fm.init({
   }
 });
 
+function showItems(category) {
+  console.log("SHOW")
+  const items = document.querySelectorAll('.warning-labels');
+  items.forEach(item => {
+    if (item.dataset.category === category) {
+      item.style.display = 'block';
+    }
+  });
+}
+function hideItems(category) {
+  console.log("HIDE")
+  const items = document.querySelectorAll('.warning-labels');
+  items.forEach(item => {
+    if (item.dataset.category === category) {
+      item.style.display = 'none';
+    }
+  });
+}
+
 function setFlow(value) {
   document.getElementById('wf').innerHTML = value + " ml/s";
 }
@@ -401,6 +420,48 @@ function GetAndShow() {
       document.getElementById('wt-value').innerText = data.WTc;
       document.getElementById('ph-value').innerText = data.pH;
       document.getElementById('co2-value').innerText = data.CO2;
+      //Environment Temp
+      if(data.ETc >= 20 && data.ETc <= 32){
+        hideItems("high-temp")
+      }
+      else{
+        showItems("high-temp")
+      }
+      //TDS
+      if(data.TDS >= 350 && data.TDS <= 600){
+        hideItems("tds-alert")
+      }
+      else{
+        showItems("tds-alert")
+      }
+      //Humidity
+      if(data.H >= 50 && data.H <= 90){
+        hideItems("hum-warning")
+      }
+      else{
+        showItems("hum-warning")
+      }
+      //Water Temp
+      if(data.WTc >= 18 && data.WTc <= 28){
+        hideItems("water-temp-warn")
+      }
+      else{
+        showItems("water-temp-warn")
+      }
+      //pH
+      if(data.pH >= 5.5 && data.pH <= 6.5){
+        hideItems("ph-warn")
+      }
+      else{
+        showItems("ph-warn")
+      }
+      //CO2
+      if(data.CO2 >= 500 && data.CO2 <= 1500){
+        hideItems("co2-warn")
+      }
+      else{
+        showItems("co2-warn")
+      }
     })
     .catch(error => {
       console.error('Error fetching data:', error);
